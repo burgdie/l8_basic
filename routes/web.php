ef6bcd03878011aebed0202ae205ecf6e199d10c
Dieter <dburgstaller@gmx.de>
2022-02-16 08:46:23 +0100

    L_45 Forgot & Reset  password  & Email verification

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,11 @@ use App\Http\Controllers\CategoryController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/email/verify', function () {
+  return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+
 Route::get('/', function () {
     return view('welcome');
 });
